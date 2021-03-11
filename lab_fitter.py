e545f40928a181389e0d2911323db1bce02054e2
arconyx <1ryanc100@gmail.com>
2021-03-11 21:42:49 +1300

    Fitting working, but quality poor

--- lab_fitter.py
+++ lab_fitter.py
@@ -8,7 +8,6 @@ Created on Thu Mar 11 17:56:32 2021
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-# import seaborn as sns
 from scipy.optimize import curve_fit
 
 # Process data into a usable format
@@ -20,6 +19,7 @@ data = raw.to_numpy()
 
 # delete first row
 data = np.delete(data, 0, 0)
+
 
 # For every temperature measurement, associates it with time and position
 def add_independents(row):
@@ -34,17 +34,32 @@ data = np.reshape(data, (-1, 3))
 #split columns into named vars for clarity
 time, x, Temperature = data.T
 
-# plt.style.use('fast')
-plt.get_cmap('viridis')
+# Plot it
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
-ax.scatter(time, x, Temperature, s=1)
-plt.draw()
+# ax.scatter(time, x, Temperature, s=1, color='black')
+ax.scatter(time, x, Temperature, s=1, c=Temperature, cmap='plasma')
+# plt.draw()
 
-def model(independent, A, d, w, C):
-    x = independent[0]
-    t = independent[1]
-    return A * np.exp(- (x / d) ) * np.sin(w*t - (x / d) )
+# Set angular frquency, given we know frequency
+f = 0.001
+w = 2 * np.pi * f
 
+# Equation to fit to
+def model(independent, A, B, C):
+    t, x = independent
+    return A * np.exp(B * x ) * np.sin(w * t - (B * x) ) +  C
 
+# Fit curve
+parameters, covariance = curve_fit(model, [time, x], Temperature)
 
+# Plot the fitted function
+sample_time = np.linspace(0, 5000, 650)
+sample_x = np.linspace(0, 0.65, 650)
+
+Time, X = np.meshgrid(sample_time, sample_x)
+
+sample_Temperature = model([Time, X], *parameters)
+
+# ax.plot_surface(Time, X, sample_Temperature, cmap='plasma', alpha=0.35)
+ax.plot_wireframe(Time, X, sample_Temperature)
